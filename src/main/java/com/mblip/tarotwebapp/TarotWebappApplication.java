fa30848b294694771c075f01e3a0872821feaf92
mdohr07 <124890015+mdohr07@users.noreply.github.com>
2024-12-04 09:39:31 +0100

    Project built

---
+++ src/main/java/com/mblip/tarotwebapp/TarotWebappApplication.java
@@ -0,0 +1,13 @@
+package com.mblip.tarotwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarotWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TarotWebappApplication.class, args);
+    }
+
+}
